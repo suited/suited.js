21b4de4f1900db31788697ef923078ae91bc255d
Karl Roberts <karl.roberts@owtelse.com>
2016-01-25 08:35:23 +1100

    added Apache License

--- src/main/assets/js/state.js
+++ src/main/assets/js/state.js
@@ -1,12 +1,27 @@
 'use strict';
+//state.js
 /**
  * The state of the system. Supports the Suited framework and keep track of the current slide and mode
  * and allos the state to be manipulated.
  * 
- * @param   {Array} modesArr Array of strings describing the available modes
  * @returns {Object}   Containing the functions necessary to check and manipulate the state
  */
 
+/*
+Copyright 2016 Karl Roberts <karl.roberts@owtelse.com> and Dirk van Rensburg <dirk.van.rensburg@gmail.com> 
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 var konstants = require('./konstants.js');
 var utils = require('./utils.js');
