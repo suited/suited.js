ad5b042f3bb40d87006f2400cad7d82bcac13ead
Karl Roberts <karl.roberts@owtelse.com>
2016-01-29 22:48:04 +1100

    linear scrolling OK.
    need to fixup virtual start slide to be slide-0
    need to merge transition into lifecycle better, ie after statechange
    after function.

--- src/main/assets/js/konstantes.js
+++ src/main/assets/js/konstantes.js
@@ -19,6 +19,60 @@ Copyright 2016 Karl Roberts <karl.roberts@owtelse.com> and Dirk van Rensburg <di
    limitations under the License.
 */
 
+
+function linearnterpolateNumber(a, b) {
+    return function (t) {
+        return a + t * (b - a);
+    };
+}
+
+function tween(startnum, endnum, durationMs, doFn, interpolator, delay) {
+
+    //close over these 
+    var ease = interpolator(startnum, endnum);
+    var startTime = (new Date()).getTime();
+    var duration = durationMs;
+
+    var endTime = startTime + durationMs;
+    var t = 0;
+    delay = (!!delay) ? delay : 0; //ms
+
+    //    console.debug("startTime=" + startTime + "  endTime = " + endTime);
+    //doFn(ease(0));
+
+
+    //@recursive
+    function _tween() {
+
+        setTimeout(function () {
+            var now = (new Date()).getTime(); // in millisecs
+
+            // bail out
+            if (now >= endTime) {
+                doFn(endnum);
+                return;
+            }
+
+            //otherwise
+            var new_t = (now - startTime) / duration;
+            var newval = ease(new_t);
+            //            console.debug("new_t=" + new_t + "  newval = " + newval);
+            doFn(newval);
+            _tween();
+        }, (delay > 0 ? delay : 0));
+    };
+
+    //kick off
+    _tween(doFn, ease);
+}
+
+
+//    var rateX = (x - (document.documentElement.scrollLeft || document.body.scrollLeft)) * m;
+//    var rateX = (x - window.scrollX) * m;
+//    var rateY = (y - (document.documentElement.scrollTop || document.body.scrollTop)) * m;
+
+
+
 var k = {
     idPrefix: "slide-",
     slideAttr: "data-slide",
@@ -62,26 +116,34 @@ k.defaultTransitions.jump.down = k.defaultTransitions.jump.right;
 k.defaultTransitions.scroll = {};
 k.defaultTransitions.scroll.left = function (elId) {
     var el = document.getElementById(elId);
-    if (el.scrollIntoView) {
-        //    if (false) {
-        el.scrollIntoView({
-            block: "end",
-            behavior: "smooth"
-        });
+    var yPos = 0;
+    var startPos = window.scrollY;
+    if (elId === "slide-0") { //hack TODO FIXME
+        yPos = -window.scrollY;
     } else {
         var rect = el.getBoundingClientRect();
-        window.scrollTo(0, rect.top);
-        //fix location bar
+        yPos = rect.top;
 
     }
 
+    //calc relative tweens and scrollTo so start at 0 go to start + ypos
+    tween(0, yPos, 400, function (y) {
+        window.scrollTo(0, startPos + y);
+    }, linearnterpolateNumber, 15)
+
+
+
 };
+
 k.defaultTransitions.scroll.right = k.defaultTransitions.scroll.left;
 k.defaultTransitions.scroll.up = k.defaultTransitions.scroll.left;
 k.defaultTransitions.scroll.down = k.defaultTransitions.scroll.left;
 
 
 k.defaultTransitions.scrollzoom = k.defaultTransitions.jump; //TODO fixme when scrollzoom implemented
+
+//k.test = tween;
+//k.ease = linearnterpolateNumber;
 
 
 
