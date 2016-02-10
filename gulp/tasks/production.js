22761660fec112c776a3c448597b386939772513
Karl Roberts <karl.roberts@owtelse.com>
2016-02-10 22:44:20 +1100

    latest gulp-starter applied. Now can produce versioned production artifacts.

--- gulp/tasks/production.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-// Run this to compress all the things!
-gulp.task('production', [], function(){
-//gulp.task('production', [], function(){
-  // This runs only if the karma tests pass
-//  gulp.start(['markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs', 'genie', 'publiclibs'])
-  gulp.start(['markup', 'images', 'iconFont', 'glyphIcons', 'minifyCss', 'browserify', 'publiclibs'])
-});
