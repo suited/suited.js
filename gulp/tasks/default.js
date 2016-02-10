cb3dc2e7a612202bc775009539c44f556657becd
Karl Roberts <karl.roberts@owtelse.com>
2016-02-10 22:44:20 +1100

    latest gulp-starter applied. Now can produce versioned production artifacts.

--- gulp/tasks/default.js
+++
@@ -1,4 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('default', ['sass', 'images', 'markup', 'glyphIcons', 'watch']);
-//gulp.task('default', ['sass', 'images', 'markup', 'publiclibs', 'watch']);
