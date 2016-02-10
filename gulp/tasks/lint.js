8b1adc5f0c563f9594f4113ac0851607743887b9
Karl Roberts <karl.roberts@owtelse.com>
2016-02-10 22:44:20 +1100

    latest gulp-starter applied. Now can produce versioned production artifacts.

--- gulp/tasks/lint.js
+++
@@ -1,9 +0,0 @@
-var gulp   = require('gulp');
-var jshint = require('gulp-jshint');
-var config = require('../config').lint;
-
-gulp.task('lint', function() {
-  return gulp.src(config.src + "/**/*.js")
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-});
