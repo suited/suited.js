d6936c872138b53440dc8e26c8fb61511e2a284b
Karl Roberts <karl.roberts@owtelse.com>
2016-04-21 18:07:12 +1000

    Lifecycle API tested.

---
+++ src/test/dummy/dummy.test.js
@@ -0,0 +1,7 @@
+describe("Dummy Test so Karma has at least one test pass to prevent Error.", function () {
+  it("contains spec with an expectation", function () {
+    var foo = true;
+    expect(foo).to.equal(true);
+  });
+
+});
