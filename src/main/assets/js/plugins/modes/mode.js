9715e4aa72b6e83ee40b3f8409c2d924bf5a2b65
Karl Roberts <karl.roberts@owtelse.com>
2016-07-22 00:20:54 +1000

    SHIFT+1 to SHIFT+9 jump directly to mode 1-9.
    Modes are now Plugins so can add callbacks to listen to events

--- src/main/assets/js/plugins/modes/mode.js
+++ src/main/assets/js/plugins/modes/mode.js
@@ -1,10 +1,9 @@
 'use strict';
-//state.js
+//mode.js
 /**
- * The state of the system. Supports the Suited framework and keep track of the current slide and mode
- * and allos the state to be manipulated.
- * 
- * @returns {Object}   Containing the functions necessary to check and manipulate the state
+ * The Mode manages the view of the document while it is active. Mode Extends Plugin so it can register with the
+ * Dispatch to act on events.
+ * The ModePlugin manages a Mode and will register and deregister it when the mode changes.
  */
 
 /*
@@ -22,13 +21,25 @@ Copyright 2016 Karl Roberts <karl.roberts@owtelse.com> and Dirk van Rensburg <di
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+
+import Plugin from '../../plugin.js'
+
+//TODO FIXME - karl - Should we allow the mode to add a callback for events for each of these functions when activated or have ModePlugin manage it
+// or have mode plugin call these funcs as currently happens? My vote is to have a function that registers them with
+// the Mode as a plugin then ModePlugin doesn't have to know anything, just manage register and deregister the Mode with the Dispatcher 
+// on mode change. What do you think?
 var Mode = function (modeName, fnBeforeSlideChange, fnAfterSlideChange, fnBeforeModeChange, fnAfterModeChange, fnCleanUp, fnShouldShowslide) {
 
-  var self = this;
-
+  var self = this; //safekeeping
+  
   if (!modeName) {
     throw "Could not instansiate mode. Mode name is not optional."
   }
+  
+  // Call the parent constructor, making sure (using Function#call)
+  // that "this" is set correctly during the call
+  Plugin.call(this, modeName);
+
 
   this.name = modeName;
 
@@ -83,5 +94,18 @@ var Mode = function (modeName, fnBeforeSlideChange, fnAfterSlideChange, fnBefore
   }
 
 };
+
+// see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
+//
+// Create a Mode.prototype object that inherits from Plugin.prototype.
+// Note: A common error here is to use "new Plugin()" to create the
+// Mode.prototype. That's incorrect for several reasons, not least 
+// that we don't have anything to give Plugin for the "name" 
+// argument. The correct place to call Plugin is above, where we call 
+// it from Mode.
+Mode.prototype = Object.create(Plugin.prototype); // See note below
+
+// Set the "constructor" property to refer to Student
+Mode.prototype.constructor = Mode;
 
 module.exports = Mode;
