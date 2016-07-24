42dad324f50b373ac5a000a34f131f2cc4b8aece
dirk.vanrensburg <dirkvanrensburg@gmail.com>
2016-07-25 00:05:18 +1000

    Fix for removeMode

--- src/test/plugin/modes/modePlugin.test.js
+++ src/test/plugin/modes/modePlugin.test.js
@@ -22,20 +22,16 @@ describe("Test mode plugin", function () {
   it("Can remove a mode", function() {
     
     var theMode = new Mode("removeMode");
-    
     var modeCount = modePlugin.modeNames.length;
+
     modePlugin.addMode(theMode);
-    
     expect(modePlugin.modeNames.length).to.equal(modeCount + 1);
     expect(modePlugin.modeNames[modeCount]).to.equal("removeMode");
     expect(modePlugin.modes["removeMode"]).to.equal(theMode);
-    
-    modePlugin.removeMode("removeNode");
+
+    modePlugin.removeMode("removeMode");
     expect(modePlugin.modeNames.length).to.equal(modeCount);
-    expect(modePlugin.modes["removeMode"]).to.be.undefined;
-    
-    
-    
+    expect(modePlugin.modes["removeMode"]).to.be.undefined;    
   });  
   
 });
