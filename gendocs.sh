1ecb07cf11aa1edccb831a2fa5e06c6b3a9b219d
Karl Roberts <karl.roberts@owtelse.com>
2016-08-02 18:24:07 +1000

    Apache 2 license header snippet on files

--- gendocs.sh
+++ gendocs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # @Author: Roberts Karl <Karl_Roberts>
 # @Date:   2016-Aug-02
 # @Project: suited
@@ -16,11 +18,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-
-
-
-#!/bin/bash
 
 rm -rf doc_site
 
