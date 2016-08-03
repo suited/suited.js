b952e388184c17055233d44d36599a9dec7a8a52
Karl Roberts <karl.roberts@owtelse.com>
2016-08-03 16:02:33 +1000

    Update 2016-08-03T06:02:16.615Z

--- markdownittest.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-// node.js, "classic" way:
-var MarkdownIt = require('markdown-it'),
-  md = new MarkdownIt();
-var result = md.render('# markdown-it rulezz!');
-console.log("result is " + result);
