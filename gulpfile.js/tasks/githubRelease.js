55edf802f7148be7015be79128d20b9f4542b66b
Karl Roberts <karl.roberts@owtelse.com>
2016-08-31 01:02:04 +1000

    githubrelease

--- gulpfile.js/tasks/githubRelease.js
+++ gulpfile.js/tasks/githubRelease.js
@@ -125,15 +125,18 @@ function getCurrentVersion() {
   }
 
 gulp.task('githubrelease', ['production','release'], function(){
+  var theToken = getToken();
+  var theVersion = getCurrentVersion();
+  var theNotes = readReleaseNotes();
   gulp.src(paths.src)
     .pipe(debug())
     .pipe(ghrelease({
-      token: getToken(),                     // or you can set an env var called GITHUB_TOKEN instead
+      token: theToken,                     // or you can set an env var called GITHUB_TOKEN instead
       owner: 'suited',                    // if missing, it will be extracted from manifest (the repository.url field)
       repo: 'suited.js',            // if missing, it will be extracted from manifest (the repository.url field)
       tag: getCurrentVersion(),                      // if missing, the version will be extracted from manifest and prepended by a 'v'
       // name: 'publish-release v1.0.0',     // if missing, it will be the same as the tag
-      notes: readReleaseNotes(),                // if missing it will be left undefined
+      notes: theNotes,                // if missing it will be left undefined
       draft: false,                       // if missing it's false
       prerelease: false                  // if missing it's false
       //manifest: require(paths.vFilePath) // package.json from which default values will be extracted if they're missing
